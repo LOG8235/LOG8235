5573dbdf41565f28cde4e341083ae510460b0441
christian <christian.akkary@outlook.com>
2026-01-18 13:10:32 -0500

    First Commit

---
+++ Source/SoftDesignTraining/SDTAIController.cpp
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "SDTAIController.h"
+#include "SoftDesignTraining.h"
+
+void ASDTAIController::Tick(float deltaTime)
+{
+	UE_LOG(LogTemp, Display, TEXT("CALLING FROM AI CONTROLLER"));
+}
+
+
+
+
